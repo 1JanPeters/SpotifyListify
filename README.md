9a934fd5ed09d35ea32e0064b1a10cc252c1a95c
1JanPeters <jan.peters@stud.h-da.de>
2022-10-14 05:26:34 +0200

    Basic album to lists functionality
    This is the commit of the initial functionality:
    Create/Fill a list with the songs of the albums a user has added to the library.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # SpotifyListify
+
+## About SpotifyListify
+
+This program uses the Spotify api to find all the songs in your library, including albums(, favorites
+and other playlists: incoming feature) and put them all in one playlist. Due to limitations from Spotify 
+the lists might have a size limit. Because of that another list gets created and filled if the previous one is full.
+
+Motivation is that you might want to load all your songs on your phone. Because the current 
+workflow does not allow to download all songs in your library easily it is very time-consuming.
+With few lists that contain every song you have in your library you can hit just a few 
+download buttons to queue all your songs to download.
+
+Another motivation is to have a list off all your songs since this basic feature is not
+supported by Spotify.
+
+## Usage
+
+- Create an app at https://developer.spotify.com
+- Copy & paste the config.ini.example and remove the '.example'
+- Next you have to replace the credentials from the ones from the spotify dashboard
+  - client_id: Client ID in the app dashboard
+  - client_secret: Also in the app dashboard
+  - userid: The userid can be retrieved from the link to the user account - For the desktop app
+  you can right-click and copy the link, then you should have a link like: 
+  https://open.spotify.com/user/userid?si=...&nd=1 for browser its just https://open.spotify.com/user/userid
+- You can leave the SPOTIPY_REDIRECT_URI as it is if you want to run SpotifyListify locally.
---
+++ main.py
@@ -0,0 +1,115 @@
+import configparser
+import time
+
+import spotipy
+from spotipy import SpotifyException
+from spotipy.oauth2 import SpotifyOAuth
+
+scope = 'playlist-read-private user-library-read playlist-modify-private'
+
+
+def choose_playlist():
+    playlists = sp.current_user_playlists()
+    name = None
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            print("%4d %s %s" % (i + playlists['offset'], playlist['uri'], playlist['name']))
+        playlist_choice = input("n for next, else number of playlist")
+        if playlist_choice.isdecimal():
+            if len(playlists) > int(playlist_choice) > -1:
+                name = playlists['items'][int(playlist_choice)]['name']
+                playlists = None
+        if playlists is not None and playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+    return name
+
+
+def find_playlist_by_name(name):
+    playlists = sp.current_user_playlists()
+    search_playlist = None
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            # print("%4d %s %s" % (i + playlists['offset'], playlist['uri'], playlist['name']))
+            if playlist['name'] == name:
+                search_playlist = playlist
+        if playlists is not None and playlists['next']:
+            playlists = sp.next(playlists)
+        else:
+            playlists = None
+    return search_playlist
+
+
+if __name__ == '__main__':
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    print(config.sections())
+    credentials = config['credentials']
+    network = config['network']
+    userid = credentials['userid']
+    client_id = credentials['client_id']
+    client_secret = credentials['client_secret']
+    SPOTIPY_REDIRECT_URI = network['SPOTIPY_REDIRECT_URI']
+
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=client_id,
+                                                   client_secret=client_secret,
+                                                   redirect_uri=SPOTIPY_REDIRECT_URI))
+    albums = sp.current_user_saved_albums()
+    index = 0
+    counter = 0
+
+    list_number = 1
+    print("Welcome to SpotifyListify, a tool to fetch all the songs in your library and put them in one list")
+    print("By doing so you can for example download the list on your phone without having to download every artist "
+          "after another")
+    print("Enter:\n"
+          "1 To select a playlist from your account\n"
+          "2 to create a new name")
+    choice=""
+    while(not choice.isdecimal()):
+        choice=input(">")
+    if choice=="1":
+        playlistName = choose_playlist()
+    if choice == "2":
+        playlistName = input("New playlist name:>")
+        sp.user_playlist_create(name=playlistName, public=False, user=userid)
+
+    all_playlist = find_playlist_by_name(playlistName)
+    if all_playlist is None:
+        exit("Could not create/find a playlist")
+    while albums:
+        uris: list = []
+        for i, album in enumerate(albums['items']):
+            tracks = sp.album_tracks(album['album']['id'])
+            while (tracks):
+                for track in tracks['items']:
+                    uris.append(track["uri"])
+                if tracks is not None and tracks['next']:
+                    tracks = sp.next(tracks)
+                else:
+                    tracks = None
+            print(i + 1, album['album']['id'], album['album']['uri'], album['album']['name'])
+            counter += len(uris)
+            try:
+                sp.playlist_add_items(playlist_id=all_playlist['id'], items=uris)
+                uris.clear()
+            except SpotifyException as ex:
+                template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                message = template.format(type(ex).__name__, ex.args)
+                print(message)
+                if str(ex.args).__contains__("Playlist size limit reached"):
+                    list_number += 1
+                    newPlaylistName = playlistName + " " + str(list_number)
+                    sp.user_playlist_create(name=newPlaylistName, public=False, user=userid)
+                    time.sleep(10)
+                    all_playlist = find_playlist_by_name(newPlaylistName)
+                    if all_playlist is None:
+                        exit("Failure at creating finding new playlist")
+                else:
+                    exit("Unexpected exception")
+        if albums['next']:
+            albums = sp.next(albums)
+        else:
+            albums = None
+    print("Number of songs ", counter)
